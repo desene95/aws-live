d5248e8fbf4eabc915788053c187c1a69e74c80c
Damian Esene <damianesene@Damians-MacBook-Air.local>
2021-03-20 22:31:12 -0400

    Commiting config and table codes

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost= “employeedb.csbmhzhflimc.ca-central-1.rds.amazonaws.com”
+Customuser = “ ”admin
+custompass = “ Ose12345!”
+customdb = “ employee“
+custombucket = “ eseneemployee“
+customeregion = “ ca-central-1“
